386ec5293e7aa0e7541c077b84cdfa0bffeaded4
罗心 <xiaoxindihua@hotmail.com>
2017-01-29 16:58:54 +0800

    setp 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,13 +2,13 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def index
       @jobs = case params[:order]
-              when 'by_lower_bound'
-                Job.published.order('wage_lower_bound DESC')
-              when 'by_upper_bound'
-                Job.published.order('wage_upper_bound DESC')
-              else
-                Job.published.recent
-              end
+      when 'by_lower_bound'
+        Job.published.order('wage_lower_bound DESC')
+      when 'by_upper_bound'
+        Job.published.order('wage_upper_bound DESC')
+      else
+        Job.published.recent
+      end
   end
   def show
     @job = Job.find(params[:id])
